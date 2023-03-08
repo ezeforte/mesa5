d5a3adbd1fe6c42e8b0e3a35dba5224ce51fed72
ezequiel3925 <51267285+ezequiel3925@users.noreply.github.com>
2023-03-08 19:38:56 -0300

    1

---
+++ 2/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  //Aqui deberias agregar los estados y los handlers para los inputs
+
+  return (
+    <div className="App">
+      <h1>Elige un color</h1>
+      <form>{/* aqui deberias escribir tu codigo */}</form>
+      <Card algo={algo} />
+    </div>
+  );
+}
+
+export default App;
---
+++ 2/src/componentes/Lista.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import { ReactDOM } from 'react';
+
+const Lista = (props) => {
+  return (
+    <>
+      <h1>{props.nome}</h1>
+
+    </>
+  )
+}
+
+export default Lista;
